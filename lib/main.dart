1c8fb1252971e5385fca35bf4bcd95ed3020fbbf
jhs512 <jangka512@gmail.com>
2024-08-19 13:41:16 +0900

    work

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'dart:io';
 
 void main() {
+  int lastId = 0;
+
   while (true) {
     stdout.write('명령) ');
-    String? command = stdin.readLineSync();
+    String command = stdin.readLineSync()!;
 
     if (command == 'exit') {
       break;
+    } else if (command == 'add') {
+      stdout.write('할일 : ');
+
+      int id = ++lastId;
+      String content = stdin.readLineSync()!;
+      print('$id번 할일이 생성되었습니다.');
     }
   }
 }
