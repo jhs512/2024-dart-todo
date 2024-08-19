e106d7cba1f2145a02e378ea4d99ba303d32c5d1
jhs512 <jangka512@gmail.com>
2024-08-19 14:01:06 +0900

    work

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 void main() {
   int lastId = 0;
+  List<Todo> todos = [];
 
   while (true) {
     stdout.write('명령) ');
@@ -9,12 +10,30 @@ void main() {
 
     if (command == 'exit') {
       break;
+    } else if (command == 'list') {
+      print("번호 / 내용");
+      for (Todo todo in todos) {
+        print('${todo.id} / ${todo.content}');
+      }
     } else if (command == 'add') {
       stdout.write('할일 : ');
 
       int id = ++lastId;
       String content = stdin.readLineSync()!;
       print('$id번 할일이 생성되었습니다.');
+
+      Todo todo = Todo(id, content);
+      todos.add(todo);
     }
+  }
+}
+
+class Todo {
+  late final int id;
+  late String content;
+
+  Todo(int id, String content) {
+    this.id = id;
+    this.content = content;
   }
 }
